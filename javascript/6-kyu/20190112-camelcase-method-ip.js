f6b2b35168df6845ae2d1a72eff281c56c46f5cb
Tea <tealalala@protonmail.com>
2019-01-12 15:53:28 -0800

    add in-progress for js > 6 kyu and 8 kyu

---
+++ javascript/6-kyu/20190112-camelcase-method-ip.js
@@ -0,0 +1,20 @@
+// 2019-01-12 Sa
+
+/////////// Working solution - not done
+
+// Codewars > JavaScript > 6 kyu
+// CamelCase Method
+// https://www.codewars.com/kata/camelcase-method
+
+// Solution 1
+String.prototype.camelCase=function(){
+  // var s = String.prototype.split(" ");
+  console.log();
+}
+
+// Tests
+"test case".camelCase(); // "TestCase"
+// "camel case method".camelCase(); // "CamelCaseMethod"
+// "say hello ".camelCase(); // "SayHello"
+// " camel case word".camelCase(); // "CamelCaseWord"
+// "".camelCase(); // ""
